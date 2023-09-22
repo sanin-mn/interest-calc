1070b8100ee1ced0159091b2df04583d16b3807f
sanin-mn <msanin7003@gmail.com>
2023-09-22 10:55:41 +0530

    validation updated

--- src/App.js
+++ src/App.js
@@ -7,22 +7,56 @@ function App() {
   const [principle, setPrinciple] = useState(0)
   const [rate, setRate] = useState(0)
   const [year, setYear] = useState(0)
+  const [validPrinciple, setValidPrinciple] = useState(true)
+  const [validRate, setValidRate] = useState(true)
+  const [validYear, setValidYear] = useState(true)
+
   const handleCalculate = (e) => {
     e.preventDefault()
     if (!principle || !rate || !year) {
       alert("Please fill the form")
     } else {
-      setInterest(principle * rate / 100 * year)
+      setInterest(Math.floor(principle * rate / 100 * year))
     }
 
   }
-  const resetData = () =>{
 
-    setInterest('')
-    setPrinciple('')
-    setRate('')
-    setYear('')
-    
+  const validateUserInput = (e) => {
+    const { name, value } = e.target
+    if (!!value.match(/^[0-9]+$/)) {
+      // valid expression
+      if (name === "principle") {
+        setPrinciple(value)
+        setValidPrinciple(true)
+      } else if (name === "rate") {
+        setRate(value)
+        setValidRate(true)
+      } else if (name === "year") {
+        setYear(value)
+        setValidYear(true)
+      }
+    } else {
+      // invalid expression
+      if (name === "principle") {
+        setPrinciple(value)
+        setValidPrinciple(false)
+      } else if (name === "rate") {
+        setRate(value)
+        setValidRate(false)
+      } else if (name === "year") {
+        setYear(value)
+        setValidYear(false)
+      }
+    }
+  }
+
+  const resetData = () => {
+
+    setInterest(0)
+    setPrinciple(0)
+    setRate(0)
+    setYear(0)
+
   }
 
   return (
@@ -41,20 +75,35 @@ function App() {
 
         <form className='mt-5' onSubmit={handleCalculate}>
           <div className='mb-3'>
-            <TextField type='number' className='w-100' id="outlined-basic1" label="₹ Principle Amount" variant="outlined"
-              value={principle || ""} onChange={(e) => setPrinciple(e.target.value)} />
+            <TextField className='w-100' id="outlined-basic1" label="₹ Principle Amount" variant="outlined"
+              value={principle || ""} name='principle' onChange={(e) => validateUserInput(e)} />
           </div>
+          {!validPrinciple &&
+            <div className='mb-3 text-danger fw-bolder'>
+              *Invalid Principle Amount
+            </div>
+          }
           <div className='mb-3'>
-            <TextField type='number' className='w-100' id="outlined-basic2" label="Rate of Interest (p.a) %" variant="outlined"
-              value={rate || ""} onChange={(e) => setRate(e.target.value)} />
+            <TextField className='w-100' id="outlined-basic2" label="Rate of Interest (p.a) %" variant="outlined"
+              value={rate || ""} name='rate' onChange={(e) => validateUserInput(e)} />
           </div>
+          {!validRate &&
+            <div className='mb-3 text-danger fw-bolder'>
+              *Invalid Rate of Interest
+            </div>
+          }
           <div className='mb-3'>
-            <TextField type='number' className='w-100' id="outlined-basic3" label="Time Period (Yr)" variant="outlined"
-              value={year || ""} onChange={(e) => setYear(e.target.value)} />
+            <TextField className='w-100' id="outlined-basic3" label="Time Period (Yr)" variant="outlined"
+              value={year || ""} name='year' onChange={(e) => validateUserInput(e)} />
           </div>
+          {!validYear &&
+            <div className='mb-3 text-danger fw-bolder'>
+              *Invalid year Input
+            </div>
+          }
           <Stack direction='row' spacing={2}>
             <Button type='submit' style={{ height: '50px', width: '250px' }} className='bg-dark' variant="contained">Calculate</Button>
-            <Button onClick={()=>resetData()} style={{ height: '50px', width: '250px' }} variant="outlined">Reset</Button>
+            <Button onClick={() => resetData()} style={{ height: '50px', width: '250px' }} variant="outlined">Reset</Button>
           </Stack>
         </form>
       </div>
